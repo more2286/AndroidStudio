e3f4a3aee13d630e132542dd232e2b6a2253262d
more2286 <moremoreno286@hotmail.com>
2017-07-17 18:37:15 -0500

    Termino Proyecto

--- LibreriasInternas/app/src/main/java/com/nextu/camc/libreriasinternas/actPrincipal.java
+++ LibreriasInternas/app/src/main/java/com/nextu/camc/libreriasinternas/actPrincipal.java
@@ -1,7 +1,11 @@
 package com.nextu.camc.libreriasinternas;
 
+import android.media.*;
+import android.provider.MediaStore.*;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.*;
+
 
 public class actPrincipal extends AppCompatActivity {
 
@@ -9,5 +13,11 @@ public class actPrincipal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lytprincipal);
+
+        Audio objAudio = new Audio();
+        Files objFile = new Files();
+        Video objVideo = new Video();
+        FaceDetector objFaceDetector = new FaceDetector(78,78,78);
+        Base64 objBase64;
     }
 }
