a45002ce6a38db7d18b4fad213fcc8cbc6e3b330
more2286 <moremoreno286@hotmail.com>
2017-07-17 17:31:03 -0500

    Termino del proyecto

--- Libreria/app/src/main/java/com/nextu/camc/libreria/actPrincipal.java
+++ Libreria/app/src/main/java/com/nextu/camc/libreria/actPrincipal.java
@@ -2,6 +2,9 @@ package com.nextu.camc.libreria;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.nextu.camc.milibreria.clsCalculdor;
 
 public class actPrincipal extends AppCompatActivity {
 
@@ -9,5 +12,7 @@ public class actPrincipal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lytprincipal);
+        double varPromedio = clsCalculdor.funFormula(9.5, 8.6, 8.4);
+        Log.i("Promedio", ((Double)varPromedio).toString());
     }
 }
---
+++ Libreria/milibreria/src/main/java/com/nextu/camc/milibreria/clsCalculdor.java
@@ -0,0 +1,11 @@
+package com.nextu.camc.milibreria;
+
+/**
+ * Created by SISTEMAS on 17/07/2017.
+ */
+
+public class clsCalculdor {
+    public static double funFormula(double parNumero1, double parNumero2, double parNumero3){
+        return (parNumero1 + parNumero2 + parNumero3) / 3;
+    }
+}
