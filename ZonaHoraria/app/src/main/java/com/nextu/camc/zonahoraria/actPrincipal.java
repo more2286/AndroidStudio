7bbe21de5c254bffbb786fc9072af660deb4d199
more2286 <moremoreno286@hotmail.com>
2017-07-24 10:52:19 -0500

    Trabajo de zona horaria linearlayout

---
+++ ZonaHoraria/app/src/main/java/com/nextu/camc/zonahoraria/actPrincipal.java
@@ -0,0 +1,13 @@
+package com.nextu.camc.zonahoraria;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class actPrincipal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lytprincipal);
+    }
+}
