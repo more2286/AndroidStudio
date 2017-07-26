069677a05dcd7f0a57ced3e58ab703aa83479147
more2286 <moremoreno286@hotmail.com>
2017-07-26 10:40:21 -0500

    Trabajo de Perfil de Usuario

---
+++ PerfilUsuario/app/src/main/java/com/nextu/camc/perfilusuario/actPrincipal.java
@@ -0,0 +1,13 @@
+package com.nextu.camc.perfilusuario;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class actPrincipal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lytprincipal);
+    }
+}
